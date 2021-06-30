f457e35ef23e8491e62351fef0381523dbb4fa6f
Raj Thombare <86057670+Raj-Thombare@users.noreply.github.com>
2021-07-01 01:22:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+var musicLibrary = {
+  Chill: [
+    { name: "Enemies", Artist: "Lauv" },
+    { name: "Aaftaab", Artist: "The Local Train" }
+  ],
+  Romantic: [
+    { name: "The Night We Met", Artist: "Lord Huron" },
+    { name: "Darasal", Artist: "Atif Aslam" }
+  ],
+  Relax: [
+    { name: "Kun Faya Kun", Artist: "Javed Ali, Mohit Chauhan" },
+    { name: "Tajdar-E-Haram", Artist: "Atif Aslam" }
+  ]
+};
+
 export default function App() {
+  const [music, setMusic] = useState("");
+
   return (
     <div className="App">
-      <h1 style={{ color: "blueviolet" }}> Feel the Music 🎵 </h1>
-      <p>Checkout my favorite songs. select genre to get started</p>
-      <button> Romantic </button>
-      <button> Rap </button>
-      <button> Happy </button>
-      <hr />
+      <h1> Feel the Music 🎵 </h1>
+      <p style={{ fontSize: "smaller" }}>
+        Checkout my favorite songs. select genre to get started
+      </p>
     </div>
   );
 }
