4fa2314c8784c7f7248402bf5ca229d705c6b012
Raj Thombare <86057670+Raj-Thombare@users.noreply.github.com>
2021-06-30 23:49:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ color: "blueviolet" }}>Feel the Music 🎵</h1>
+      <h2>Edit to see some magic happen!</h2>
+    </div>
+  );
+}
