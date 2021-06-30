d892a868a592ac233c72babe22ef20c7b2fdb03c
Raj Thombare <86057670+Raj-Thombare@users.noreply.github.com>
2021-07-01 00:04:32 +0530

    style: done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1 style={{ color: "blueviolet" }}>Feel the Music 🎵</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h1 style={{ color: "blueviolet" }}> Feel the Music 🎵 </h1>
+      <p>Checkout my favorite songs. select genre to get started</p>
+      <button> Romantic </button>
+      <button> Rap </button>
+      <button> Happy </button>
+      <hr />
     </div>
   );
 }
